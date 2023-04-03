24ec89b2de5c3edf478b6319a61887b90cdd7c25
Tien Nhan <nguyentiennhan9a2@gmail.com>
2023-04-03 13:36:25 +0700

    as

--- script.js
+++ script.js
@@ -68,4 +68,31 @@ let interval = null;
    }, 30);   } 
    
    document.querySelector("#cac").ontransitionstart = hackingFunc;  
-   document.querySelector("#cac").onmouseover = hackingFunc;  
+   document.querySelector("#cac").onmouseover = hackingFunc; 
+   
+   var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    centeredSlides: false,
+    slidesPerGroupSkip: 1,
+    grabCursor: true,
+    keyboard: {
+      enabled: true,
+    },
+    breakpoints: {
+      769: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+      },
+    },
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
